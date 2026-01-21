1807ca9e65a61643bc59ae0968609af40538e80e
Dhruv Bhatt <dhruvbhatt77777@gmail.com>
2026-01-21 10:12:29 +0530

    Day 3: Add Python functions, lists, and loops

---
+++ day-3/README.md
@@ -0,0 +1,7 @@
+# Week 1 – Day 3
+
+Topics Covered:
+- Python functions and reusable code
+- Lists in Python
+- for-loop implementation
+- Calling functions with arguments
---
+++ day-3/src/functions.py
@@ -0,0 +1,12 @@
+def greet_user(name):
+    print("Hello", name)
+
+def print_numbers(numbers):
+    for num in numbers:
+        print(num)
+
+
+greet_user("Dhruv")
+
+numbers_list = [1, 2, 3, 4, 5]
+print_numbers(numbers_list)
